54f17b5d0468c0b470a3b7576bcd9b81306ef250
Louie S <louie@example.com>
2024-06-21 16:21:48 -0400

    Modularize get_title

--- src/index-pages.sh
+++ src/index-pages.sh
@@ -2,27 +2,20 @@
 
 # shellcheck source=../../../scripts/create_table.sh
 . "$(dirname "$0")"/../../../scripts/create_table.sh
+# shellcheck source=../../../scripts/get_title.sh
+. "$(dirname "$0")"/../../../scripts/get_title.sh
 # shellcheck source=../../../scripts/insert.sh
 . "$(dirname "$0")"/../../../scripts/insert.sh
 
 DB_PATH="$1"
 shift
 
-get_title() {
-	FILE="$1"
-
-	pup -p -f "$FILE" 'title text{}' | \
-		tr -d \\n | \
-		sed 's/(GNU make)//g' | \
-		sed 's/\"/\"\"/g'
-}
-
 insert_pages() {
 	# Get title and insert into table for each html file
 	while [ -n "$1" ]; do
 		unset PAGE_NAME
 		unset PAGE_TYPE
-		PAGE_NAME="$(get_title "$1")"
+		PAGE_NAME="$(get_title "$1" | sed 's/(GNU make)//g')"
 		PAGE_TYPE="Guide"
 		if [ -n "$PAGE_NAME" ]; then
 			insert "$DB_PATH" "$PAGE_NAME" "$PAGE_TYPE" "$(basename "$1")"
